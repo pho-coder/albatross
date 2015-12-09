7f9513a77f74183a60efb41b3e302457262c2ffc
phoenix <pho.coderzhang@gmail.com>
2015-12-09 21:19:36 +0800

    fix

--- test/com/jd/bdp/magpie/albatross/util/server_test.clj
+++
@@ -1,7 +0,0 @@
-(ns com.jd.bdp.magpie.albatross.util.server-test
-  (:require [clojure.test :refer :all]
-            [com.jd.bdp.magpie.albatross.util.server :refer :all]))
-
-(deftest start-server-test
-  (testing
-      (is (= true (start-server)))))

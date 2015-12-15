90b5dcdd693ca3b32e7abb2cf66cb07c0d4dbd0b
zengguangyao <zengguangyao@jd.com>
2015-12-15 15:41:53 +0800

    实现 config parser

--- project.clj
+++ project.clj
@@ -3,6 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["jd-libs-releases" "http://artifactory.360buy-develop.com/libs-releases"]
+                 ["jd-libs-snapshots" "http://artifactory.360buy-develop.com/libs-snapshots"]
+                 ["jd-plugins-releases" "http://artifactory.360buy-develop.com/plugins-releases"]
+                 ["jd-plugins-snapshots" "http://artifactory.360buy-develop.com/plugins-snapshots"]]
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
@@ -11,6 +16,8 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [thrift-clj "0.3.0"]
                  [clj-zookeeper "0.2.0-SNAPSHOT"]
+
+                 [com.jd.bdp.buffalo.sdk/bdp-buffalo-sdk "1.0.4-SNAPSHOT"]
                  [com.jd.bdp.magpie/magpie-framework-clj "0.1.0-SNAPSHOT"]
                  [com.jd.bdp.magpie/magpie-utils "0.1.3-SNAPSHOT"]]
   :main com.jd.bdp.magpie.albatross.core
--- src/clj/com/jd/bdp/magpie/albatross/conf_parser.clj
+++ src/clj/com/jd/bdp/magpie/albatross/conf_parser.clj
@@ -1,4 +1,22 @@
-(ns com.jd.bdp.magpie.albatross.conf-parser)
+(ns com.jd.bdp.magpie.albatross.conf-parser
+  (:require [clojure.tools.logging :as log])
+  (:import (com.jd.extservice.impl PlumberServiceImpl)
+           (com.jd.exception BuffaloException)))
+
+(defn- template-bean->map
+  [abean]
+  (let [amap (bean abean)
+        ext-target-ds (bean (:extTargetDs amap))
+        ext-src-ds-list (map bean (:extSrcDsList amap))]
+    (assoc amap :extTargetDs ext-target-ds :extSrcDsList ext-src-ds-list)))
+
+(defn- get-template-bean
+  [template-id]
+  (let [ps (PlumberServiceImpl.)
+        template-bean (try
+                        (.getTemplateById ps template-id)
+                        (catch BuffaloException e (.printStackTrace e) (log/error e) nil))]
+    (template-bean->map template-bean)))
 
 (defn parse
   "return struct: 
@@ -6,6 +24,9 @@
   {task-id [;; one thread in task one map
             {:source source :target target :sql sql}]}"
   [job-id]
+  (get-template-bean job-id)
+
+  ;临时返回的测试值
   {"test-task-0" (list {:source "mysql"
                         :target "hadoop"
                         :sql "select * from a"})

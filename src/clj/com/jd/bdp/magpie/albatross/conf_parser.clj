0aec58dbcaf9e2a6b29e15b113e84c9c3e9e9e44
zengguangyao <zengguangyao@jd.com>
2016-01-13 18:41:49 +0800

    完成config parser

--- src/clj/com/jd/bdp/magpie/albatross/conf_parser.clj
+++ src/clj/com/jd/bdp/magpie/albatross/conf_parser.clj
@@ -10,8 +10,21 @@
            (com.jd.model.plumber ExtPlumberTemplateBean)))
 
 (def SEPARATOR "*p*")
+
+(defn- get-dictionary-value
+  [key]
+  )
+
+(defn- generate-base-conf
+  []
+  )
+
+(defn- group-sqls
+  [sql-list]
+  (partition 10 sql-list))
+
 ; TODO 以下信息来自哪里？
-(def BASE-CONF {:jar "magpie-mysql2hadoop-plumber-task-0.0.1-SNAPSHOT-standalone.jar"
+(def BASE-CONF {:jar "magpie-mysql2hadoop-plumber-task-0.0.2-SNAPSHOT-standalone.jar"
                 :klass "com.jd.bdp.magpie.magpie_eggs_clj.magpie_mysql2hadoop_plumber_task.core"
                 :group "default"
                 :type "memory"})
@@ -19,15 +32,6 @@
 (defn- generate-task-name
   [albatross-id job-id uuid]
   (str "plumber" SEPARATOR albatross-id SEPARATOR job-id SEPARATOR uuid))
-
-(defn template-bean->map
-  " 因为bean方法不能迭代地执行，把JavaBean内部的JavaBean变成Map
-    没有找到合适的方法之前，只好用原始手段构造"
-  [abean]
-  (let [amap (bean abean)
-        ext-target-ds (bean (:extTargetDs amap))
-        ext-src-ds-list (map bean (:extSrcDsList amap))]
-    (assoc amap :extTargetDs ext-target-ds :extSrcDsList ext-src-ds-list)))
 
 (defn get-template-bean
   [template-id]
@@ -105,7 +109,7 @@
             _ (log/info "@@@@@@@@@ dbName :" dbName " dsEnable : " dsEnable " dsSql : " source-sql " final-sql : " final-sql)
             real-sqls (template/get-sqls dbType final-sql dbHost dbName data-date true dt)
             _ (log/info "real sqls : " real-sqls)]
-        (doseq [real-sql real-sqls]
+        (doseq [real-sql (group-sqls real-sqls)]
           (log/info "real-sql : " real-sql)
           (let [sub-job-id (generate-task-name ALBATROSS-SERVICE-NAME job-id (util/uuid))
                 base-conf BASE-CONF
@@ -139,62 +143,12 @@
             (swap! sub-jobs conj sub-job)))))
     @sub-jobs))
 
-(defn- generate-task-conf
-  "return a vector: [task-confs task-num]"
-  [job-id task-list]
-  (reduce (fn [[tasks i] item]
-            (let [task-id (generate-task-name ALBATROSS-SERVICE-NAME job-id i)
-                  conf (into BASE-CONF item)]
-              [(assoc tasks task-id conf) (inc i)]))
-          [{} 1]
-          task-list))
-
-(defn- split-conf-to-list
-  [template-bean job-id]
-  (let [extSrcDsList (:extSrcDsList template-bean)
-        afn (fn [src] (assoc template-bean :extSrcDsList [src]))  ; 保持conf结构与原来一致，但extSrcList只有一个元素
-        task-list (map afn extSrcDsList)
-        [confs _] (generate-task-conf job-id task-list)]
-    confs))
-
 (defn parse
   "return struct:
   strategy-fn return a map :
   {task-id [;; one thread in task one map
             {:source source :target target :sql sql :jar jar :klass klass :group group :type type}}"
   [job-id]
-  ; (strategy-fn (get-template-bean job-id) job-id)
-  ; TODO 以下在测试时使用
-  (let [conf-list (split-conf-to-list (template-bean->map (get-template-bean job-id)) job-id)]
-    (map (fn [[task-id conf]]
-           (let [ext-tar-ds (:extTargetDs conf)
-                 target-table (:targetTableName conf)
-                 sql (:sqlStr conf)
-                 target-path (str (:extend1 ext-tar-ds) "/" target-table)
-                 ext-src-ds (first (:extSrcDsList conf))
-                 host (:dbHost ext-src-ds)
-                 user (:dbUser ext-src-ds)
-                 password (:dbPassword ext-src-ds)
-                 src-db-name (:dbName ext-src-ds)
-                 source (:dbType ext-src-ds)]
-             ; 最终的配置
-             {task-id {:source source
-                       :target target-path
-                       :host host
-                       :sqls [sql]
-                       :db-name src-db-name
-                       :user user
-                       :password password}}))
-         conf-list)))
-
-
-(defn parse
-  "return struct:
-  strategy-fn return a map :
-  {task-id [;; one thread in task one map
-            {:source source :target target :sql sql :jar jar :klass klass :group group :type type}}"
-  [job-id]
-  ; (strategy-fn (get-template-bean job-id) job-id)
   ; TODO 以下在测试时使用
   (let [template-bean (get-template-bean job-id)]
     (log/info "config" template-bean)
